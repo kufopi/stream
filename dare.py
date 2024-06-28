1b47c153c11527d1be2442f3fbf1546ef56f3a93
Kuforiji <kufopo@gmail.com>
2024-06-28 22:51:27 +0100

    Update dare.py

--- dare.py
+++ dare.py
@@ -212,11 +212,12 @@ def program_run():
     ppdf = persons_df.copy()
     ppdf.reset_index(drop=True, inplace=True)
     common_column ='Student_name'
-    merged_df = data.loc[data[common_column].isin(ppdf[common_column])]    
+    merged_df = data.loc[data[common_column].isin(ppdf[common_column])] 
+    
 
     
     st.dataframe(ppdf)
-    st.dataframe(merged_df)
+    st.dataframe(remanat_df[remanat_df['Student_name'].isin(persons_df['Student_name'])]    )
     common_columns = [col for col in data.columns if col in persons_df.columns]    
     st.write(common_columns)    
         
